0edd169cadbf6c5bfee013099be2c11c0a4e5df7
Liza Ng <lng@pivotal.io>
2018-02-10 17:14:16 +0800

    Initial commit

---
+++ src/main/java/com/example/s3properties/S3PropertiesApplication.java
@@ -0,0 +1,12 @@
+package com.example.s3properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3PropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3PropertiesApplication.class, args);
+    }
+}
